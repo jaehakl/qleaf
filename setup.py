d573b330f79417ec2c61b1faf2127b3bf55d3adc
active0831 <leejaehak87@gmail.com>
2024-08-23 23:15:12 +0900

    0.0.12
    proc

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='qleaf',
-    version='0.0.11',
+    version='0.0.12',
     description='Productive GUI Framework for Python',
     author='Jaehak Lee',
     author_email='leejaehak87@gmail.com',
